e8ea6e386197c6ec0187152ffaedfed2cebe906b
Austin Harshberger <aharshbe@Austins-MacBook-Pro.local>
2016-04-14 09:44:48 -0700

    Solving add button variable

--- To_DoList2/app/src/main/java/com/example/aharshbe/to_dolist2/MainActivity.java
+++ To_DoList2/app/src/main/java/com/example/aharshbe/to_dolist2/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.addButton);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
